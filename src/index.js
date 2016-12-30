96ec8fb4c3048e4bd16906140f2efb44d265f20b
Kyle Kelley <rgbkrk@gmail.com>
2016-12-29 17:10:15 -0800

    useless lodash flow types -> we're on lodash/fp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const red = require('./reducers')
+global.red = red;
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
